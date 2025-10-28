8db82f1fb0d0ea836b9fbe0b06be834da7c91b19
kkyung2 <dnrud4354@naver.com>
2025-10-28 11:13:52 +0900

    수정1차

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Ki_Portfolio/", // GitHub Pages 경로
+  base: "/", // GitHub Pages 경로
 });
