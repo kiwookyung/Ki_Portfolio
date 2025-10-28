76c70b60c35d784918ffa9502b964204680411d3
kkyung2 <dnrud4354@naver.com>
2025-10-28 10:23:17 +0900

    chore: 코드 포맷팅

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Ki_Portfolio/', // GitHub Pages 경로
-})
+  base: "/Ki_Portfolio/", // GitHub Pages 경로 - Repository 이름과 일치해야 함
+});
