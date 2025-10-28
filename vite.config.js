9fa4262f9487e90288c639753f922a07976e661e
kkyung2 <dnrud4354@naver.com>
2025-10-28 10:37:49 +0900

    feat: gh-pages 방식으로 배포 설정 변경

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Ki_Portfolio/", // GitHub Pages 경로 - Repository 이름과 일치해야 함
+  base: process.env.NODE_ENV === "production" ? "/Ki_Portfolio/" : "/", // GitHub Pages 경로
 });
