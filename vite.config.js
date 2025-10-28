cabff33409d3eb616696bf4c73e20fa0962ba598
kkyung2 <dnrud4354@naver.com>
2025-10-28 10:54:59 +0900

    fix: vite base 경로 명시적 설정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/Ki_Portfolio/" : "/", // GitHub Pages 경로
+  base: "/Ki_Portfolio/", // GitHub Pages 경로
 });
