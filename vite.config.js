c8d30df8e86950d58964148e828b43680912dc0d
kkyung2 <dnrud4354@naver.com>
2025-10-28 11:19:59 +0900

    fix:경로수정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // GitHub Pages 경로
+  base: "/", // GitHub Pages 경로 - kiwookyung.github.io로 배포하려면 리포지토리 이름을 kiwookyung.github.io로 변경
 });
